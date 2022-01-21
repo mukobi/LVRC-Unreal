d099c60e13402264cd2f26c571a0dabd2f490ed9
Gabriel Mukobi <gabrielmukobi@gmail.com>
2022-01-21 09:37:16 -0800

    Turn the project into a code project with an empty class

---
+++ LVRC_Project/Source/LVRC_Project/LVRC_Project.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "LVRC_Project.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, LVRC_Project, "LVRC_Project" );
